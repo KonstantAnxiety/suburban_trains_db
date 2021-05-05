84345a1737df36b368e2d818e8fd3422f3b6779a
Alexey Konstantinov <askonstantinov@miem.hse.ru>
2021-05-05 19:49:45 +0300

    Fixed .gitignore

---
+++ GUI/scripts/GUI_main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class MainWindow(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        btn_edit_dialog = tk.Button(self, text='Жопа', bg='#ffffff', bd=2, command=self.destroy)
+        btn_edit_dialog.pack()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = MainWindow(root)
+    app.pack()
+    root.title('Ж О П А')
+    root.geometry("800x600+100+100")
+    root.mainloop()
